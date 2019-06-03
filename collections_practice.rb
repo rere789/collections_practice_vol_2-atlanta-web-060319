43cd0e2a98f7f0f7ded9ed1f56dc5845f5f9ae19
Reina Ewing <reina.castro3@gmail.com>
2019-06-03 19:09:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
 # your code goes here
+require "pry"
+
+def begins_with_r(array)
+    array.all? do |word|
+      word[0].include?("r")
+  end 
+end 
+
+def contain_a(array)
+  new_array = []
+    array.each do |word|
+      if word.include?("a")
+      new_array << word 
+    end 
+  end 
+    return new_array
+end
+
+
+def first_wa(array)
+  new_array = []
+    array.detect do |word|
+      if word.detect("wa")
+        new_array << word
+      end
+    end 
+    return new_array
+end 
